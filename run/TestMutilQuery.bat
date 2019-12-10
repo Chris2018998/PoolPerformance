6700e88a5cf81e95577a1c83fa7b8086ec39d1ca
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2019-12-10 22:20:52 +0800

    Update TestMutilQuery.bat

--- run/TestMutilQuery.bat
+++ run/TestMutilQuery.bat
@@ -6,7 +6,7 @@ set CLASSPATH=.
 FOR %%i IN ("%folder%/lib/*.jar") DO CALL "setClassPath.bat" %folder%/lib/%%i   
 
 set JAVA_OPTS= -Xms2048m -Xmx2048m
-%JAVA_HOME%\bin\java cn.bee.dbcp.test.MutilThreadQuery
+%JAVA_HOME%\bin\java cn.beecp.test.MutilThreadQuery
 
 
  
